19b0f883f4d58993511f25017012771584c742af
zxb <3375902062@qq.com>
2026-05-07 22:06:15 +0800

    refactor: polish auth user response

---
+++ src/main/java/com/zhq/taskforge/auth/vo/UserInfoResponseVo.java
@@ -0,0 +1,11 @@
+package com.zhq.taskforge.auth.vo;
+
+import lombok.Data;
+
+@Data
+public class UserInfoResponseVo {
+
+    private Long userId;
+    private String name;
+    private String nickName;
+}
--- src/main/java/com/zhq/taskforge/model/system/controller/UserController.java
+++ src/main/java/com/zhq/taskforge/model/system/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.zhq.taskforge.model.system.controller;
 
+import com.zhq.taskforge.auth.vo.UserInfoResponseVo;
 import com.zhq.taskforge.common.Result;
 import com.zhq.taskforge.common.ResultCode;
+import com.zhq.taskforge.security.LoginUser;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "用户模块")
 public class UserController {
 
+
+
     @GetMapping("/me")
     @Operation(summary = "获取当前登录用户的信息")
-    public Result getCurrentUserInfo(@AuthenticationPrincipal UserDetails userDetails) {
+    public Result getCurrentUserInfo(@AuthenticationPrincipal LoginUser userDetails) {
         if (userDetails == null) {
             return new Result(ResultCode.SUCCESS, "用户未登录");
         }
+        UserInfoResponseVo userInfoResponseVo = new UserInfoResponseVo();
+        userInfoResponseVo.setName(userDetails.getUsername());
+        userInfoResponseVo.setNickName(userDetails.getNickName());
+        userInfoResponseVo.setUserId(userDetails.getUserId());
         return new Result(ResultCode.SUCCESS,userDetails);
     }
 
